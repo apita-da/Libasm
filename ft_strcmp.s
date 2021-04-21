13823a54e90be95dffcdb2fc3657d4855c1f13c3
apita-da <amaliapdvh@gmail.com>
2021-04-21 14:03:20 +0200

    strcmp start

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,20 +1,24 @@
 section .text
 	global _ft_strcmp
 
+__ft_strcmp:				;rsi = s1, rdi = s2
+		xor		rcx, rcx	;i = 0;
+		cmp		rsi, 0		;rsi existe
+		cmp		rdi, 0		;rdi existe
 
 
 
 
-int		ft_strcmp(char *s1, char *s2)
-{
-	int cont;
+;int		ft_strcmp(char *s1, char *s2)
+;{
+;	int i;
 
-	cont = 0;
-	while ((s1[cont] != '\0') || (s2[cont] != '\0'))
-	{
-		if (s1[cont] != s2[cont])
-			return (s1[cont] - s2[cont]);
-		cont++;
-	}
-	return (0);
-}
+;	i = 0;
+;	while ((s1[i] != '\0') || (s2[i] != '\0'))
+;	{
+;		if (s1[i] != s2[i])
+;			return (s1[i] - s2[i]);
+;		i++;
+;	}
+;	return (0);
+;}
